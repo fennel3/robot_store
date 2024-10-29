e769a764478e316244172ef638a624c62d942f42
Finn <finley.champion@gmail.com>
2024-10-29 15:48:22 +0000

    added structures

---
+++ src/DatabaseConnector.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace src;
+
+class DatabaseConnector
+{
+    public static function connect()
+    {
+        $db = new PDO(
+            'mysql:host=DB;dbname=robot_stores',
+            'root',
+            'password'
+        );
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    }
+}
---
+++ src/Entities/Items.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src\Entities;
+
+class Items
+{
+    public string $id;
+    public string $title;
+    public string $price;
+    public string $image;
+    public string $category_id;
+    public string $category;
+    public string $character_id;
+    public string $character;
+    public string $description;
+    public string $image2;
+    public string $image3;
+
+}
