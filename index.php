b75f5a250bd43a024f8a11f72947515439fd29dd
Finn <finley.champion@gmail.com>
2024-10-30 20:06:27 +0000

    added the ability to click the div like its a button which posts the items id

--- index.php
+++ index.php
@@ -9,12 +9,20 @@ $db = DatabaseConnector::connect();
 
 $product = ProductHydrator::getProducts($db);
 
-foreach ($product as $item){
-    echo "<div class='product'>";
-    echo "<h1>" . htmlspecialchars($item->title) . "</h1>";
-    echo "<p>Price" . htmlspecialchars($item->price) . "</p>";
-    echo "<img src='" . htmlspecialchars($item->image) . "' alt='" . htmlspecialchars($item->title) . "' />";
+
+
+foreach ($product as $item) {
+    echo "<form method='POST' style='display: inline-block;'>";
+    echo "<input type='hidden' name='product_id' value='" . htmlspecialchars($item->id) . "' />";
+    echo "<button type='submit' style='all: unset; display: block; height: 200px; width: 100px; cursor: pointer;'>";
+    echo "<div class='product' style='height: 100%; width: 100%; overflow: hidden;'>";
+    echo "<h1 style='font-size: 14px; margin: 0;'>" . htmlspecialchars($item->title) . "</h1>";
+    echo "<p style='font-size: 12px; margin: 0;'>Price: $" . htmlspecialchars($item->price) . "</p>";
+    echo "<img src='" . htmlspecialchars($item->image) . "' alt='" . htmlspecialchars($item->title) . "' style='width: 100%; height: auto;' />";
     echo "</div>";
+    echo "</button>";
+    echo "</form>";
+
 
 }
 
