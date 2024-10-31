0c3c4f736444993c1c0f7abd59d1ae6b9edcc6fe
Finn <finley.champion@gmail.com>
2024-10-31 16:01:25 +0000

    product details spits out json details

--- getProduct.php
+++ getProduct.php
@@ -4,7 +4,6 @@
 require ('vendor/autoload.php');
 
 
-
 use RobotStores\DatabaseConnector;
 use RobotStores\Hydrators\ProductHydrator;
 use RobotStores\Services\PageDisplay;
@@ -12,41 +11,22 @@ use RobotStores\Services\PageDisplay;
 
 $db = DatabaseConnector::connect();
 
-$products = ProductHydrator::getProducts($db);
-
-
-
+$product = ProductHydrator::getProducts($db);
 
 //POST body contents (JSON) get put into this
-$json = file_get_contents('php://input');
-
+//$json = file_get_contents('php://input');
 //once we get the contents we must decode them
-$data = json_decode($json, true);
-
-
+//$data = json_decode($json, true);
 
 //data is now an associative array
 // this will allow us to access it from a superglobal
-$_POST = $data;
+//$_POST = $data;
+
+// now we can put the $data in the DB if we w
 
 
+header('Content-Type: application/json');
 
+echo json_encode($product);
 
-
-if (!empty($_POST)) {
-    $output = [
-        'success' => true,
-        'message' => 'Success!',
-        'request_data' => $data,
-    ];
-    http_response_code(201);
-    echo json_encode($output);
-} else {
-    $output = [
-        'success' => false,
-        'message' => 'It didn\'t work'
-    ];
-    http_response_code(500);
-    echo json_encode($output);
-}
 
